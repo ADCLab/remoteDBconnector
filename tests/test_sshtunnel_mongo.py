4c64fc0ca7d3524483b5d3aac19f0d510b127960
aevela <44214575+aevela@users.noreply.github.com>
2021-09-15 22:13:23 -0400

    Update test_sshtunnel_mongo.py
    Remove configpaser from test code

--- tests/test_sshtunnel_mongo.py
+++ tests/test_sshtunnel_mongo.py
@@ -1,10 +1,7 @@
 from remoteDBconnector import dbconn
-from configparser import ConfigParser
 import pandas
 
 
-config = ConfigParser()
-config.read('myconfexample_mongo.cfg')
 mytunnel=dbconn('myconfexample_mongo.cfg')
 # a=pandas.read_sql('SHOW databases;',mytunnel.conn)
 
